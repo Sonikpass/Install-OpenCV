47848ede446d6185ab9ce8e3b704af42baade941
Jason Mullings <jason@sonikpass.com>
2017-06-03 10:42:18 -0700

    Added darwin

---
+++ Darwin/opencv_install.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# Dan Walkes
+# 2014-01-29
+# Call this script after configuring variables:
+# version - the version of OpenCV to be installed
+# downloadfile - the name of the OpenCV download file
+# dldir - the download directory (optional, if not specified creates an OpenCV directory in the working dir)
+if [[ -z "$version" ]]; then
+    echo "Please define version before calling `basename $0` or use a wrapper like opencv_latest.sh"
+    exit 1
+fi
+if [[ -z "$downloadfile" ]]; then
+    echo "Please define downloadfile before calling `basename $0` or use a wrapper like opencv_latest.sh"
+    exit 1
+fi
+if [[ -z "$dldir" ]]; then
+    dldir=OpenCV
+fi
+if ! sudo true; then
+    echo "You must have root privileges to run this script."
+    exit 1
+fi
+set -e
+
+echo "--- Installing OpenCV" $version
+
+echo "--- Installing Dependencies"
+source dependencies.sh
+
+echo "--- Downloading OpenCV" $version
+mkdir -p $dldir
+cd $dldir
+if [ ! -f $downloadfile ]; then
+    echo "Downloading OpenCV" $version
+    wget -O $downloadfile http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/$version/$downloadfile/download
+fi
+#wget -c -O $downloadfile http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/$version/$downloadfile/download
+
+echo "--- Installing OpenCV" $version
+echo $downloadfile | grep ".zip"
+if [ $? -eq 0 ]; then
+    unzip $downloadfile
+else
+    tar -xvf $downloadfile
+fi
+cd opencv-$version
+mkdir build
+cd build
+cmake 	-D WITH_1394=OFF -D BUILD_DOCS=NO -DENABLE_PRECOMPILED_HEADERS=OFF -D OPENCV_EXTRA_MODULES_PATH=/home/ec2-user/opencv_contrib/opencv_contrib-3.1.0/modules -D CMAKE_BUILD_TYPE=RELEASE -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF -D CMAKE_INSTALL_PREFIX=/usr/local ..
+
+
+make -j 4
+sudo make install
+sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
+sudo ldconfig
+echo "OpenCV" $version "ready to be used"
